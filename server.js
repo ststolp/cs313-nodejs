158ea8ad948d16926472f801a2300f4c96971037
Devi Damsel <scstolp@gmail.com>
2019-03-07 15:31:40 -0700

    adding redirection

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.use(express.static("public"));
 app.set("views", "view");
 app.set("view engine", "ejs");
 
+app.get("/", redirectUser);
+
+function redirectUser(req, res) {
+    return res.redirect('home.html');
+};
+
 app.get("/post_form", calculate);
 app.listen(process.env.PORT, function(){
 	console.log("It's working");
