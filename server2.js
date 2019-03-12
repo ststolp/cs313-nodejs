1ab735e110a7b674ee4f06f28a106b820cdc6a98
Devi Damsel <scstolp@gmail.com>
2019-03-12 11:07:54 -0600

    finished

--- server2.js
+++ server2.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
 
+const { Pool } = require('pg');
+
+const connection = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: connection});
+
 app.use(express.static("public"));
 app.set("views", "view");
 app.set("view engine", "ejs");
 
+app.get("/getPerson", function(req, res) {
+     let id = req.query.id;
+     let sql = "SELECT * FROM person WHERE id = $1";
+     pool.query = (sql, [id], function(error, result) {
+         if (error) {
+             console.log("There was an error");
+            } else {
+                console.log("The result is " + result.rows[0]);
+            }
+     });     
+
+})
 app.listen(process.env.PORT, function(){
 	console.log("It's working");
 });
