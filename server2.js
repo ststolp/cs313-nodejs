aabc9f6f2f6d065b485dad7531c2c9e57a4dcb14
Devi Damsel <scstolp@gmail.com>
2019-03-15 12:23:28 -0600

    fix

--- server2.js
+++ server2.js
@@ -1,27 +1,29 @@
 var express = require('express');
 var app = express();
 
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const connection = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connection});
-
+    
 app.use(express.static("public"));
 app.set("views", "view");
 app.set("view engine", "ejs");
 
-app.get("/getPerson", function(req, res) {
-     let id = req.query.id;
-     let sql = "SELECT * FROM person WHERE id = $1";
-     pool.query = (sql, [id], function(error, result) {
-         if (error) {
-             console.log("There was an error");
-            } else {
-                console.log("The result is " + result.rows[0]);
-            }
-     });     
+app.get("/getPerson", getPerson);
 
-})
 app.listen(process.env.PORT, function(){
 	console.log("It's working");
 });
+
+ function getPerson(req, res) {
+    let id = req.query.id;
+    let sql = "SELECT * FROM person WHERE id = $1";
+    pool.query = (sql, [id], function(error, result) {
+        if (error) {
+            console.log("There was an error");
+          } 
+        console.log("The result is " + result.rows);
+        res.json(result);
+        });
+}   
